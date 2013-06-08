4598ebd7c9485830a5f9b54af714fa7363d0c3aa
Watke <wsd.2012.ken@gmail.com>
2013-06-08 23:13:42 +0300

    merge from master

--- public/test/javascripts/roomtest.js
+++ public/test/javascripts/roomtest.js
@@ -3,62 +3,63 @@
  * Date: 10/04/2013
  * Time: 13:31
  */
-define(['../../javascripts/room.js','../../javascripts/Constant.js'],function (Room,CONSTANT) {
-        return {
-            RunTests: function () {
-                module('Room');
-                asyncTest('Enter the room',function(){
-                    function successCB(sess){
-                        equal(sess instanceof ab.Session,true);
-                        equal(sess._websocket.protocol,"wamp");
-                        start();
-                    }
+/*global define, asyncTest, deepEqual, start, ok, equal*/
+define(['../../javascripts/room.js',
+        '../../javascripts/Constant.js',
+        '../../javascripts/autobahn.js'], function (Room, CONSTANT, ab) {
+    "use strict";
+    return {
+        RunTests: function () {
+            module('Room');
+            asyncTest('Enter the room', function () {
+                function successCB(sess) {
+                    equal(sess instanceof ab.Session, true);
+                    /*jslint nomen: true*/
+                    equal(sess._websocket.protocol, "wamp");
+                    /*jslint nomen: false*/
+                    start();
+                }
 
-                    function errorCB (err) {
-                        // if put assert here will cause error when exit the server
-                        ok(false,err);
-                        start();
-                    }
+                function errorCB(err) {
+                    // if put assert here will cause error when exit the server
+                    ok(false, err);
+                    start();
+                }
 
-                    var room = new Room();
-                    room.comeIn(successCB,errorCB);
-                });
+                var room = new Room();
+                room.comeIn(successCB, errorCB);
+            });
 
-                asyncTest('Switch the Light', function(){
-                    // enter the room callbacks
-                    function successCB_1 (sess){
-                        ok(true);
-                        room.switchLight(onSwitchedLight,rpcSuccessCB_2,errorCB_2);
-                    }
+            asyncTest('Switch the Light', function () {
+                var room = new Room();
 
-                    function errorCB_1 (err) {
-                        // if put assert here will cause error when exit the server
-                        console.log(err);
-                    }
+                function onSwitchedLight(topicUri, data) {
+                    equal(Object.prototype.toString.call(data), '[object Array]');
+                    equal(data[0], 'Light Status');
+                    equal(topicUri, CONSTANT.WAMP.TOPIC.LIGHT_STATUS);
+                    start();
+                }
+                // switch light callback
+                function rpcSuccessCB_2(data) {
+                    equal(typeof data, 'boolean');
+                }
+                function errorCB_2(err) {
+                    console.log(err);
+                    ok(false);
+                    start();
+                }
+                // enter the room callbacks
+                function successCB_1(sess) {
+                    ok(true);
+                    room.switchLight(onSwitchedLight, rpcSuccessCB_2, errorCB_2);
+                }
+                function errorCB_1(err) {
+                    // if put assert here will cause error when exit the server
+                    console.log(err);
+                }
+                room.comeIn(successCB_1, errorCB_1);
+            });
 
-                    // switch light callback
-                    function rpcSuccessCB_2 (data){
-                        equal(typeof data, 'boolean');
-                    }
-
-                    function errorCB_2 (err) {
-                        console.log(err);
-                        ok(false);
-                        start();
-                    }
-
-                    function onSwitchedLight(topicUri, data) {
-                        equal(Object.prototype.toString.call(data),'[object Array]');
-                        equal(data[0],'Light Status');
-                        equal(topicUri,CONSTANT.WAMP.TOPIC.LIGHT_STATUS);
-                        start();
-                    }
-
-                    var room = new Room();
-                    room.comeIn(successCB_1,errorCB_1);
-                });
-
-            }
         }
-    }
-);
+    };
+});
--- public/test/javascripts/suntest.js
+++ public/test/javascripts/suntest.js
@@ -3,37 +3,44 @@
  * Date: 23/04/2013
  * Time: 15:39
  */
-define(['../../javascripts/sun.js',
-        '../../javascripts/Constant.js'],function(Sun,CONSTANT){
+/*global define, asyncTest, deepEqual, start, ok, equal, test*/
+define(['jqeury',
+        '../../javascripts/sun.js',
+        '../../javascripts/Constant.js',
+        '../../javascripts/autobahn.js'], function ($, Sun, CONSTANT, ab) {
+    "use strict";
     return {
         RunTests : function () {
             module('Sun');
 
-            asyncTest('init',function(){
+            asyncTest('init', function () {
                 var theSun = new Sun();
-                theSun.init(successCB,errorCB);
-
                 function successCB(session) {
-                    equal(session instanceof ab.Session,true);
-                    equal(session._websocket.protocol,"wamp");
+                    equal(session instanceof ab.Session, true);
+                    /*jslint nomen: true*/
+                    equal(session._websocket.protocol, "wamp");
+                    /*jslint nomen: false*/
                     start();
                 }
-                function errorCB (err) {
-                    ok(false,err);
+                function errorCB(err) {
+                    ok(false, err);
                     start();
                 }
+                theSun.init(successCB, errorCB);
             });
 
             module('Sun Period');
-            test('sun rise',function(){
-                var theSun = new Sun();
-                var aSunPeriod = new theSun.SunPeriod();
+            test('sun rise', function () {
+                var i,
+                    sunBurst,
+                    sunBurst_new,
+                    theSun = new Sun(),
+                    aSunPeriod = new theSun.SunPeriod();
 
-                $('.sunburst b').css('opacity',0);
+                $('.sunburst b').css('opacity', 0);
                 // NOTE : the 9th time should go to sunset, so here set to 8
-                for (var i = 0 ; i < 8 ; i ++) {
-                    var sunBurst = Math.abs($('.sunburst b').css('opacity'));
-                    var sunBurst_ori = sunBurst;
+                for (i = 0; i < 8; i += 1) {
+                    sunBurst = Math.abs($('.sunburst b').css('opacity'));
 
                     // make sure sunBurst is in range
                     sunBurst = aSunPeriod.sunBurstConverter(sunBurst);
@@ -42,24 +49,25 @@ define(['../../javascripts/sun.js',
                     aSunPeriod.sunRise(sunBurst);
 
                     // current sunburst
-                    var sunBurst_new = Math.abs($('.sunburst b').css('opacity'));
+                    sunBurst_new = Math.abs($('.sunburst b').css('opacity'));
 
                     sunBurst_new = aSunPeriod.sunBurstConverter(sunBurst_new);
-                    sunBurst_ori = aSunPeriod.sunBurstConverter(sunBurst_ori);
-                    equal(sunBurst_ori < sunBurst_new, true);
+                    sunBurst = aSunPeriod.sunBurstConverter(sunBurst);
+                    equal(sunBurst < sunBurst_new, true);
                 }
 
             });
 
-            test('sun set',function(){
-                var theSun = new Sun();
-                var aSunPeriod = new theSun.SunPeriod();
+            test('sun set', function () {
+                var i,
+                    sunBurst,
+                    sunBurst_new,
+                    theSun = new Sun(),
+                    aSunPeriod = new theSun.SunPeriod();
 
-                $('.sunburst b').css('opacity',1);
-                for (var i = 0 ; i < 8 ; i ++) {
-                    var sunBurst = Math.abs($('.sunburst b').css('opacity'));
-
-                    var sunBurst_ori = sunBurst;
+                $('.sunburst b').css('opacity', 1);
+                for (i = 0; i < 8; i += 1) {
+                    sunBurst = Math.abs($('.sunburst b').css('opacity'));
 
                     // make sure sunBurst is in range
                     sunBurst = aSunPeriod.sunBurstConverter(sunBurst);
@@ -67,23 +75,24 @@ define(['../../javascripts/sun.js',
                     aSunPeriod.sunSet(sunBurst);
 
                     // current sunburst
-                    var sunBurst_new = Math.abs($('.sunburst b').css('opacity'));
+                    sunBurst_new = Math.abs($('.sunburst b').css('opacity'));
 
                     sunBurst_new = aSunPeriod.sunBurstConverter(sunBurst_new);
-                    sunBurst_ori = aSunPeriod.sunBurstConverter(sunBurst_ori);
-                    equal(sunBurst_ori > sunBurst_new, true);
+                    sunBurst = aSunPeriod.sunBurstConverter(sunBurst);
+                    equal(sunBurst > sunBurst_new, true);
                 }
             });
 
-            test('sun config - 1',function(){
-                var theSun = new Sun();
-                var aSunPeriod = new theSun.SunPeriod();
+            test('sun config - 1', function () {
+                var options,
+                    theSun = new Sun(),
+                    aSunPeriod = new theSun.SunPeriod();
 
-                equal(aSunPeriod.isContinued,true);
-                equal(aSunPeriod.timeInterval,800);
-                equal(aSunPeriod.isSunRise,true);
+                equal(aSunPeriod.isContinued, true);
+                equal(aSunPeriod.timeInterval, 800);
+                equal(aSunPeriod.isSunRise, true);
 
-                var options = {
+                options = {
                     isContinued : false,
                     timeInterval : 100,
                     isSunRise : false
@@ -91,67 +100,68 @@ define(['../../javascripts/sun.js',
 
                 aSunPeriod.config(options);
 
-                equal(aSunPeriod.isContinued,false);
-                equal(aSunPeriod.timeInterval,100);
-                equal(aSunPeriod.isSunRise,false);
+                equal(aSunPeriod.isContinued, false);
+                equal(aSunPeriod.timeInterval, 100);
+                equal(aSunPeriod.isSunRise, false);
 
                 aSunPeriod.reset();
 
-                equal(aSunPeriod.isContinued,CONSTANT.SUN.IS_CONTINUED);
-                equal(aSunPeriod.timeInterval,CONSTANT.SUN.TIME_INTERVAL);
-                equal(aSunPeriod.isSunRise,CONSTANT.SUN.IS_SUN_RISE);
+                equal(aSunPeriod.isContinued, CONSTANT.SUN.IS_CONTINUED);
+                equal(aSunPeriod.timeInterval, CONSTANT.SUN.TIME_INTERVAL);
+                equal(aSunPeriod.isSunRise, CONSTANT.SUN.IS_SUN_RISE);
             });
 
-            test('sun config - 2',function(){
-                var theSun = new Sun();
-                var aSunPeriod = new theSun.SunPeriod();
+            test('sun config - 2', function () {
+                var options,
+                    theSun = new Sun(),
+                    aSunPeriod = new theSun.SunPeriod();
 
-                equal(aSunPeriod.isContinued,true);
-                equal(aSunPeriod.timeInterval,800);
-                equal(aSunPeriod.isSunRise,true);
+                equal(aSunPeriod.isContinued, true);
+                equal(aSunPeriod.timeInterval, 800);
+                equal(aSunPeriod.isSunRise, true);
 
-                var options = {
+                options = {
                     isContinued : false,
                     isSunRise : true
                 };
 
                 aSunPeriod.config(options);
 
-                equal(aSunPeriod.isContinued,false);
-                equal(aSunPeriod.timeInterval,800); // should not change
-                equal(aSunPeriod.isSunRise,true);
+                equal(aSunPeriod.isContinued, false);
+                equal(aSunPeriod.timeInterval, 800); // should not change
+                equal(aSunPeriod.isSunRise, true);
 
                 aSunPeriod.reset();
 
-                equal(aSunPeriod.isContinued,CONSTANT.SUN.IS_CONTINUED);
-                equal(aSunPeriod.timeInterval,CONSTANT.SUN.TIME_INTERVAL);
-                equal(aSunPeriod.isSunRise,CONSTANT.SUN.IS_SUN_RISE);
+                equal(aSunPeriod.isContinued, CONSTANT.SUN.IS_CONTINUED);
+                equal(aSunPeriod.timeInterval, CONSTANT.SUN.TIME_INTERVAL);
+                equal(aSunPeriod.isSunRise, CONSTANT.SUN.IS_SUN_RISE);
             });
 
-            asyncTest('sun start',function(){
-                var theSun = new Sun();
-                var aSunPeriod = new theSun.SunPeriod();
+            asyncTest('sun start', function () {
+                var options,
+                    theSun = new Sun(),
+                    aSunPeriod = new theSun.SunPeriod(),
+                    counter = 8,
+                    loop_counter = 0;
+                function callback(sunBurst) {
+                    if (counter === 0) {
+                        aSunPeriod.isContinued = false;
+                        equal(loop_counter, 8);
+                        start();
+                    }
+                    counter -= 1;
+                    loop_counter += 1;
+                }
 
-                var options = {
+                options = {
                     timeInterval : 100
                 };
                 // just make it faster
                 aSunPeriod.config(options);
 
-                var counter = 8;
-                var loop_counter = 0;
                 aSunPeriod.start(callback);
-
-                function callback(sunBurst){
-                    if (counter == 0){
-                        aSunPeriod.isContinued = false;
-                        equal(loop_counter,8);
-                        start();
-                    }
-                    counter--;
-                    loop_counter++;
-                }
             });
         }
-    }
+    };
 });
