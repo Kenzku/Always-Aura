e1fdea783e76ce23e3a27ba612a6f1d8e61e1ed2
Watke <wsd.2012.ken@gmail.com>
2013-06-08 20:21:30 +0300

    merge from master

--- public/javascripts/light.js
+++ public/javascripts/light.js
@@ -3,116 +3,118 @@
  * Date: 11/04/2013
  * Time: 17:49
  */
-define(['../javascripts/api/lightactuatorapi.js',
-    '../javascripts/initwamp.js',
-    '../javascripts/Constant.js'],function(LightActuatorAPI,init,CONSTANT){
- return Light;
-});
+/*global define*/
+define(['jqeury',
+        '../javascripts/api/lightactuatorapi.js',
+        '../javascripts/initwamp.js',
+        '../javascripts/Constant.js'], function ($, LightActuatorAPI, init, CONSTANT) {
+    "use strict";
+    function Light() {
+        var self = this;
 
-function Light() {
-    var self = this;
+        self.sess = null;
 
-    self.sess = null;
+        self.currentLightStatus = null;
+        /**
+         * establish a WAMP connection with the server, and subscribe the default
+         * topic: "room:lightStatus"
+         * @param onLightStatusChange (topic, event) called a WAMP event comes - typeID : 8
+         * This type is for publish/subscribe mode and it means that one client,
+         * who subscribe the same channel has published a message. In this case,
+         * the client should be the room, who has the switch.
+         * the data structure is {'turnLightTo': data}
+         * @param onSunStatusChange (strength)
+         * @param successCallback (session) called when init is successfully done
+         * @param errorCallback (error) called when an error happen
+         */
+        self.init = function (onLightStatusChange, onSunStatusChange, successCallback, errorCallback) {
+            // call on WAMP connection successfully establish
+            function successCB(session) {
+                self.sess = session;
+                self.sess.prefix("room", "http://" + CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/room#");
+                self.sess.prefix("sun", "http://" + CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/sun#");
 
-    self.currentLightStatus = null;
-    /**
-     * establish a WAMP connection with the server, and subscribe the default
-     * topic: "room:lightStatus"
-     * @param onLightStatusChange (topic, event) called a WAMP event comes - typeID : 8
-     * This type is for publish/subscribe mode and it means that one client,
-     * who subscribe the same channel has published a message. In this case,
-     * the client should be the room, who has the switch.
-     * the data structure is {'turnLightTo': data}
-     * @param onSunStatusChange (strength)
-     * @param successCallback (session) called when init is successfully done
-     * @param errorCallback (error) called when an error happen
-     */
-    self.init = function(onLightStatusChange, onSunStatusChange, successCallback, errorCallback){
-        // init WAMP connection
-        init(successCB,errorCB);
-
-        // call on WAMP connection successfully establish
-        function successCB(session){
-            self.sess = session;
-            self.sess.prefix("room", "http://"+CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/room#");
-            self.sess.prefix("sun", "http://"+CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/sun#");
-
-            self.sess.subscribe(CONSTANT.WAMP.TOPIC.LIGHT_STATUS, onLightStatusChange);
+                self.sess.subscribe(CONSTANT.WAMP.TOPIC.LIGHT_STATUS, onLightStatusChange);
 //            self.senseSun(onSunStatusChange);
 
-            if (successCallback && typeof successCallback === 'function'){
-                successCallback (session);
+                if (successCallback && typeof successCallback === 'function') {
+                    successCallback(session);
+                }
             }
-        }
-        // call on WAMP connection failed on establish
-        function errorCB(error){
-            if (errorCallback && typeof errorCallback === 'function'){
-                errorCallback(error);
-            }else{
-                throw error;
+            // call on WAMP connection failed on establish
+            function errorCB(error) {
+                if (errorCallback && typeof errorCallback === 'function') {
+                    errorCallback(error);
+                } else {
+                    throw error;
+                }
             }
-        }
-    }
-    /**
-     * check the current light status
-     * @param rpcSuccessCallback (Object : currentLightStatus) light status before switching,
-     * it is the current light status {isLightOn:Boolean, strength: Number}
-     * @param rpcErrorCallback (error) the reason of error
-     */
-    self.lightStatus = function (rpcSuccessCallback, rpcErrorCallback){
-        var aLightActuatorAPI = new LightActuatorAPI();
-        if (self.sess){
-            aLightActuatorAPI.lightStatus(self.sess, rpcSuccessCallback, rpcErrorCallback);
-        }else{
-            if (rpcErrorCallback && typeof rpcErrorCallback === 'function'){
-                rpcErrorCallback(CONSTANT.ERROR.LIGHT.LIGHT_STATUS);
-            }else{
-                throw CONSTANT.ERROR.LIGHT.LIGHT_STATUS;
+            // init WAMP connection
+            init(successCB, errorCB);
+        };
+        /**
+         * check the current light status
+         * @param rpcSuccessCallback (Object : currentLightStatus) light status before switching,
+         * it is the current light status {isLightOn:Boolean, strength: Number}
+         * @param rpcErrorCallback (error) the reason of error
+         */
+        self.lightStatus = function (rpcSuccessCallback, rpcErrorCallback) {
+            var aLightActuatorAPI = new LightActuatorAPI();
+            if (self.sess) {
+                aLightActuatorAPI.lightStatus(self.sess, rpcSuccessCallback, rpcErrorCallback);
+            } else {
+                if (rpcErrorCallback && typeof rpcErrorCallback === 'function') {
+                    rpcErrorCallback(CONSTANT.ERROR.LIGHT.LIGHT_STATUS);
+                } else {
+                    throw CONSTANT.ERROR.LIGHT.LIGHT_STATUS;
+                }
             }
-        }
-    }
-    /**
-     * newStatus {Boolean} isLightOn | {Number} strength
-     */
-    self.updateLightUI = function (newStatus){
-        switch (typeof newStatus) {
-            case 'boolean' :
+        };
+        /**
+         * newStatus {Boolean} isLightOn | {Number} strength
+         */
+        self.updateLightUI = function (newStatus) {
+            var body = $('body');
+            function onOffMode(newStatus) {
+                body.removeAttr('style');
+                if (newStatus) {
+                    body.attr('class', 'lightOn');
+                    $('p').attr('class', 'textLightOn');
+                } else {
+                    body.attr('class', 'lightOff');
+                    $('p').attr('class', 'textLightOff');
+                }
+            }
+
+            function dimmer(newStatus) {
+                if (newStatus > 1) {
+                    var colour = "radial-gradient(circle at center, rgb(251, 255, 58) " + newStatus + "%, rgb(254, 255, 209) " + newStatus * 2 + "%)";
+                    body.attr('class', 'lightOn');
+                    $('p').attr('class', 'textLightOn');
+                    $('.lightOn').css('background-image', colour);
+                } else {
+                    body.removeAttr('style');
+                    body.attr('class', 'lightOff');
+                    $('p').attr('class', 'textLightOff');
+                }
+            }
+            switch (typeof newStatus) {
+            case 'boolean':
                 onOffMode(newStatus);
                 break;
-            case 'number' :
+            case 'number':
                 dimmer(newStatus);
-        }
-
-        function onOffMode(newStatus) {
-            $('body').removeAttr('style');
-            if (newStatus) {
-                $('body').attr('class', 'lightOn');
-                $('p').attr('class', 'textLightOn');
-            } else {
-                $('body').attr('class', 'lightOff');
-                $('p').attr('class', 'textLightOff');
+                break;
             }
-        }
+        };
 
-        function dimmer(newStatus){
-            if (newStatus > 1) {
-                var colour = "radial-gradient(circle at center, rgb(251, 255, 58) " + newStatus+ "%, rgb(254, 255, 209) " + newStatus * 2 + "%)"
-                $('body').attr('class', 'lightOn');
-                $('p').attr('class', 'textLightOn');
-                $('.lightOn').css('background-image',colour);
-            } else {
-                $('body').removeAttr('style');
-                $('body').attr('class', 'lightOff');
-                $('p').attr('class', 'textLightOff')
-            }
-        }
-    }
+        self.doNotSenseSun = function () {
+            self.sess.unsubscribe(CONSTANT.WAMP.TOPIC.SUN);
+        };
 
-    self.doNotSenseSun = function(){
-        self.sess.unsubscribe(CONSTANT.WAMP.TOPIC.SUN);
+        self.senseSun = function (onSunStatusChange) {
+            self.sess.subscribe(CONSTANT.WAMP.TOPIC.SUN, onSunStatusChange);
+        };
     }
-
-    self.senseSun = function(onSunStatusChange){
-        self.sess.subscribe(CONSTANT.WAMP.TOPIC.SUN, onSunStatusChange);
-    }
-}
+    return Light;
+});
--- public/javascripts/mainlight.js
+++ public/javascripts/mainlight.js
@@ -9,39 +9,34 @@ require.config({
         'constant' : '/javascripts/Constant'
     }
 });
-require(['light','constant'],function(Light,CONSTANT){
+require(['light', 'constant'], function (Light, CONSTANT) {
+    "use strict";
     var aLight = new Light();
-    aLight.init(onLightStatusChange,null,successCB_1,errorCB);
-
-    function successCB_1(){
-        aLight.lightStatus(successCB_2,errorCB);
-    }
-    // lightStatus: {isLightOn: Boolean, strength: Number}
-    function successCB_2(lightStatus){
-        aLight.updateLightUI(lightStatus.isLightOn);
-        if (lightStatus.isLightOn){
-            // light on
-            aLight.senseSun(onSunStatusChange);
+    function onSunStatusChange(topic, event) {
+        if (topic === CONSTANT.WAMP.TOPIC.SUN &&
+                Object.prototype.toString.call(event) === '[object Array]' &&
+                event.length === 4) {
+            console.log(aLight.currentLightStatus);
+            if (aLight.currentLightStatus) {
+                aLight.updateLightUI(event[2].turnLightTo);
+            }
         }
     }
-    function errorCB (error){
-        console.log(error);
-    }
     function onLightStatusChange(topic, event) {
-        if (topic == CONSTANT.WAMP.TOPIC.LIGHT_STATUS &&
-            Object.prototype.toString.call(event) == '[object Array]' &&
-            event.length == 4){
+        if (topic === CONSTANT.WAMP.TOPIC.LIGHT_STATUS &&
+                Object.prototype.toString.call(event) === '[object Array]' &&
+                event.length === 4) {
 
-            if(typeof event[2].turnLightTo === 'boolean'){
+            if (typeof event[2].turnLightTo === 'boolean') {
 
                 aLight.currentLightStatus = event[2].turnLightTo;
                 console.log('onLightStatusChange: ' + aLight.currentLightStatus);
 
-                if(aLight.currentLightStatus){
+                if (aLight.currentLightStatus) {
                     // light on
                     console.log('sub');
                     aLight.senseSun(onSunStatusChange);
-                }else{
+                } else {
                     console.log('unsub');
                     aLight.doNotSenseSun();
                 }
@@ -50,15 +45,19 @@ require(['light','constant'],function(Light,CONSTANT){
             aLight.updateLightUI(event[2].turnLightTo);
         }
     }
-    function onSunStatusChange(topic, event) {
-        if (topic == CONSTANT.WAMP.TOPIC.SUN &&
-            Object.prototype.toString.call(event) == '[object Array]' &&
-            event.length == 4){
-            console.log(aLight.currentLightStatus);
-            if(aLight.currentLightStatus){
-                aLight.updateLightUI(event[2].turnLightTo);
-            }
+    // lightStatus: {isLightOn: Boolean, strength: Number}
+    function successCB_2(lightStatus) {
+        aLight.updateLightUI(lightStatus.isLightOn);
+        if (lightStatus.isLightOn) {
+            // light on
+            aLight.senseSun(onSunStatusChange);
         }
     }
-
+    function errorCB(error) {
+        console.log(error);
+    }
+    function successCB_1() {
+        aLight.lightStatus(successCB_2, errorCB);
+    }
+    aLight.init(onLightStatusChange, null, successCB_1, errorCB);
 });
