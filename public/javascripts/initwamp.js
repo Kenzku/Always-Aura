7dcf110bcf6c67c649c16fd3dba54d8598054fc5
Watke <wsd.2012.ken@gmail.com>
2013-06-08 20:13:36 +0300

    merge from master

--- public/javascripts/initwamp.js
+++ public/javascripts/initwamp.js
@@ -3,27 +3,29 @@
  * Date: 10/04/2013
  * Time: 11:39
  */
-define(['../javascripts/autobahn.js'],function(ab){
+/*global define*/
+define(['../javascripts/autobahn.js'], function (ab) {
+    "use strict";
+    function init(successCallback, errorCallback) {
+        var PORT = "3000";
+
+        // connect to WAMP server
+        ab.connect("ws://localhost:" + PORT,
+
+            // WAMP session was established
+            function (session) {
+                // {{ab.Session}} things to do once the session has been established
+                console.log("Connected!");
+                successCallback(session);
+            },
+
+            // WAMP session is gone: failure, closing, or brake-off
+            function (code, reason) {
+                // things to do once the session fails
+                console.log(reason);
+                errorCallback(reason);
+            }
+            );
+    }
     return init;
 });
-function init (successCallback, errorCallback){
-    var PORT = "3000";
-
-    // connect to WAMP server
-    ab.connect("ws://localhost:" + PORT,
-
-        // WAMP session was established
-        function (session) {
-            // {{ab.Session}} things to do once the session has been established
-            console.log("Connected!");
-            successCallback(session);
-        },
-
-        // WAMP session is gone: failure, closing, or brake-off
-        function (code, reason) {
-            // things to do once the session fails
-            console.log(reason);
-            errorCallback(reason);
-        }
-    );
-}
