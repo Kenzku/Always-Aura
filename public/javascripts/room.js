ca6ea70e7f6bb826dc9aee2709372344f13ae16d
Watke <wsd.2012.ken@gmail.com>
2013-06-08 20:33:00 +0300

    merge from master

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -3,105 +3,102 @@
  * Date: 10/04/2013
  * Time: 11:52
  */
-define(['../javascripts/api/lightactuatorapi.js',
+/*global define*/
+define(['jqeury',
+        '../javascripts/api/lightactuatorapi.js',
         '../javascripts/initwamp.js',
         '../javascripts/api/componentapi.js',
-        '../javascripts/Constant.js'],
-function(LightActuatorAPI,init, ComponentAPI,CONSTANT){
-   return Room;
+        '../javascripts/Constant.js'], function ($, LightActuatorAPI, init, ComponentAPI, CONSTANT) {
+    "use strict";
+    function Room() {
+        var self = this;
+
+        self.sess = CONSTANT.WAMP.SESSION.DEFAULT;
+
+        self.comeIn = function (successCallback, errorCallback) {
+            /**
+             * when successfully initialise the component
+             * @param res {String} rpc callback value
+             */
+            function onSwitchReady(res) {
+                if (successCallback && typeof successCallback === 'function') {
+                    successCallback(self.sess);
+                }
+            }
+            // call on WAMP connection successfully establish
+            function successCB(session) {
+                self.sess = session;
+                self.sess.prefix("room", "http://" + CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/room#");
+                self.sess.prefix("actuator", "http://" + CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/actuator#");
+
+                // initComponent
+                var aComponentAPI = new ComponentAPI();
+                aComponentAPI.initComponent(self.sess,
+                    'switch',
+                    {switchMode: CONSTANT.COMPONENT_SPEC.DEFAULT.SWITCH_MODE.DIMMER},
+                    onSwitchReady, errorCallback);
+            }
+            // call on WAMP connection failed
+            function errorCB(error) {
+                if (errorCallback && typeof errorCallback === 'function') {
+                    errorCallback(error);
+                } else {
+                    throw error;
+                }
+            }
+            // init WAMP connection
+            init(successCB, errorCB);
+        };
+        /**
+         * the room can turn on the light, because it has a switch
+         * @param onSwitchedLight (topicUri, data) where the data define in /lib/handlers.js
+         * @param successCallback (boolean) the status of the light, after switch
+         * @param errorCallback (error) the reason of error
+         */
+        self.switchLight = function (onSwitchedLight, successCallback, errorCallback) {
+            var aLightActuatorAPI = new LightActuatorAPI();
+            /**
+             * call when knows t
+             * @param data {Boolean} light status after switching
+             */
+            function rpcSuccessCallback(data) {
+                // exclude me: according to the standard
+                aLightActuatorAPI.adjust(self.sess, data);
+                if (successCallback && typeof successCallback === 'function') {
+                    successCallback(data);
+                }
+            }
+            aLightActuatorAPI.switchLight(self.sess, onSwitchedLight, rpcSuccessCallback, errorCallback);
+        };
+
+        self.switchUI = function (onSwitchedLight, successCallback, errorCallback) {
+            $(document).on('click', '#lightSwitch', self.switchLight);
+        };
+
+        self.adjustUI = function (onAdjustLight, rpcSuccessCallback, rpcErrorCallback) {
+            var aLightActuatorAPI = new LightActuatorAPI();
+            /**
+             * because of UI simulation only ranging from 0 to 30
+             * @returns {Number} the real strength of luminance
+             */
+            function converter() {
+                /*jslint nomen: true*/
+                var _strength = $('#dimmer').val(),
+                    strength = (10 * _strength) / 3;
+                /*jslint nomen: false*/
+                return parseInt(strength, 10);
+            }
+            // for efficiency, final state only
+            $(document).on('mouseup', '#dimmer', function (event) {
+                // set On/off status on CouchDB
+                aLightActuatorAPI.adjustLuminance(self.sess, converter(), rpcSuccessCallback, rpcErrorCallback);
+            });
+            $(document).on('change', '#dimmer', function (event) {
+                // this will not effect CouchDB
+                aLightActuatorAPI.adjust(self.sess, parseInt($('#dimmer').val(), 10));
+            });
+        };
+
+    }
+    return Room;
 });
-
-function Room() {
-    var self = this;
-
-    self.sess = CONSTANT.WAMP.SESSION.DEFAULT;
-
-    self.comeIn = function (successCallback,errorCallback) {
-        // init WAMP connection
-        init(successCB,errorCB);
-
-        // call on WAMP connection successfully establish
-        function successCB(session){
-            self.sess = session;
-            self.sess.prefix("room", "http://"+CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/room#");
-            self.sess.prefix("actuator", "http://"+CONSTANT.DOMAIN + ":" + CONSTANT.PORT +  "/actuator#");
-
-            // initComponent
-            var aComponentAPI = new ComponentAPI();
-            aComponentAPI.initComponent(self.sess,
-                                        'switch',
-                                        {switchMode:CONSTANT.COMPONENT_SPEC.DEFAULT.SWITCH_MODE.DIMMER},
-                                        onSwitchReady,errorCallback);
-        }
-
-        /**
-         * when successfully initialise the component
-         * @param res {String} rpc callback value
-         */
-        function onSwitchReady (res){
-            if (successCallback && typeof successCallback === 'function'){
-                successCallback(self.sess);
-            }
-        }
-
-        // call on WAMP connection failed
-        function errorCB(error){
-            if (errorCallback && typeof errorCallback === 'function'){
-                errorCallback(error);
-            }else{
-                throw error;
-            }
-        }
-    }
-
-    /**
-     * the room can turn on the light, because it has a switch
-     * @param onSwitchedLight (topicUri, data) where the data define in /lib/handlers.js
-     * @param successCallback (boolean) the status of the light, after switch
-     * @param errorCallback (error) the reason of error
-     */
-    self.switchLight = function (onSwitchedLight, successCallback, errorCallback){
-        var aLightActuatorAPI = new LightActuatorAPI();
-        aLightActuatorAPI.switchLight(self.sess, onSwitchedLight, rpcSuccessCallback, errorCallback);
-
-        /**
-         * call when knows t
-         * @param data {Boolean} light status after switching
-         */
-        function rpcSuccessCallback (data) {
-            // exclude me: according to the standard
-            aLightActuatorAPI.adjust(self.sess,data);
-            if (successCallback && typeof successCallback === 'function'){
-                successCallback(data);
-            }
-        }
-    }
-
-    self.switchUI = function (onSwitchedLight, successCallback, errorCallback){
-        $(document).on('click','#lightSwitch',self.switchLight);
-    }
-
-    self.adjustUI = function (onAdjustLight, rpcSuccessCallback, rpcErrorCallback){
-        var aLightActuatorAPI = new LightActuatorAPI();
-        // for efficiency, final state only
-        $(document).on('mouseup','#dimmer',function(event){
-
-            // set On/off status on CouchDB
-            aLightActuatorAPI.adjustLuminance(self.sess,converter(),rpcSuccessCallback,rpcErrorCallback);
-        });
-        $(document).on('change','#dimmer',function(event){
-            // this will not effect CouchDB
-            aLightActuatorAPI.adjust(self.sess,parseInt($('#dimmer').val()));
-        });
-        /**
-         * because of UI simulation only ranging from 0 to 30
-         * @returns {Number} the real strength of luminance
-         */
-        function converter (){
-            var _strength = $('#dimmer').val();
-            var strength = ( 10 * _strength ) / 3;
-            return parseInt(strength);
-        }
-    }
-
-}
