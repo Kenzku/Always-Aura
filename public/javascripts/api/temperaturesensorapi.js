fcca2c404900416b3b9270d98f0a66afdb95c794
Watke <wsd.2012.ken@gmail.com>
2013-06-08 13:58:20 +0300

    code quality control, JSLint

--- public/javascripts/api/temperaturesensorapi.js
+++ public/javascripts/api/temperaturesensorapi.js
@@ -5,39 +5,41 @@
  * NOTE: initwamp.js should have been called before hand,
  * so that the WAMP connection has been setup.
  */
-define(function(){
-    return TemperatureSensorAPI;
-});
-
-function TemperatureSensorAPI() {
-    var self = this;
-
-    /**
-     * get data from a sensor
-     * @param callback success callback
-     * with the returned data as its parameter
-     */
-    self.getData = function (session,successCallback,errorCallback) {
-        if (successCallback && typeof successCallback === 'function'){
-            successCB.successCallback = successCallback;
-        }
-        if (errorCallback && typeof errorCallback === 'function'){
-            errorCB.errorCallback = errorCallback;
-        }
-
-        session.call('sensor:getData').then(successCB,errorCB);
+/*global define*/
+define(function () {
+    "use strict";
+    function TemperatureSensorAPI() {
+        var self = this;
 
         /**
-         * RPC success callback
-         * @param res the data that the server transfer
+         * get data from a sensor
+         * with the returned data as its parameter
+         * @param session WAMP session
+         * @param successCallback with parameter (response)
+         * @param errorCallback with parameter (error)
          */
-        function successCB (res){
-            console.log(res);
-            successCB.successCallback(res);
-        }
-        function errorCB (err) {
-            console.log(err);
-            errorCB.errorCallback(err);
-        }
+        self.getData = function (session, successCallback, errorCallback) {
+            /**
+             * RPC success callback
+             * @param res the data that the server transfer
+             */
+            function successCB(res) {
+                console.log(res);
+                successCB.successCallback(res);
+            }
+            function errorCB(err) {
+                console.log(err);
+                errorCB.errorCallback(err);
+            }
+            if (successCallback && typeof successCallback === 'function') {
+                successCB.successCallback = successCallback;
+            }
+            if (errorCallback && typeof errorCallback === 'function') {
+                errorCB.errorCallback = errorCallback;
+            }
+
+            session.call('sensor:getData').then(successCB, errorCB);
+        };
     }
-}
+    return TemperatureSensorAPI;
+});
