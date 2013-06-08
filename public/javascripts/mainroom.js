f972dcd9cba3e44ea83cae73cd61351051135523
Watke <wsd.2012.ken@gmail.com>
2013-06-08 20:23:26 +0300

    merge from master

--- public/javascripts/mainroom.js
+++ public/javascripts/mainroom.js
@@ -8,33 +8,22 @@ require.config({
         'room' : '/javascripts/room'
     }
 });
-require(['room'],function(){
+require(['room'], function (Room) {
+    "use strict";
     var aRoom = new Room();
-
-    aRoom.comeIn(successCB_1,errorCB_1);
-
-    function successCB_1(sess){
-        aRoom.switchUI(onSwitchedLight,rpcSuccessCB_2,errorCB_2);
-        aRoom.adjustUI(onAdjustLight, rpcSuccessCB_2, errorCB_2);
-    }
-    function errorCB_1 (err) {
-        console.log(err);
-    }
-
     // switch light callback
-    function rpcSuccessCB_2 (data){
+    function rpcSuccessCB_2(data) {
         console.log("rpcSuccessCB_2: ");
     }
 
-    function errorCB_2 (err) {
+    function errorCB_2(err) {
         console.log(err);
     }
 
-    function successCB_3 (topicUri, data){
+    function successCB_3(topicUri, data) {
         console.log("successCB_3: " + data);
         console.log("successCB_3: " + topicUri);
     }
-
     function onSwitchedLight(topicUri, data) {
         console.log("onSwitchedLight: " + data);
         console.log("onSwitchedLight: " + topicUri);
@@ -43,5 +32,12 @@ require(['room'],function(){
         console.log("onSwitchedLight: " + data);
         console.log("onSwitchedLight: " + topicUri);
     }
-
+    function successCB_1(sess) {
+        aRoom.switchUI(onSwitchedLight, rpcSuccessCB_2, errorCB_2);
+        aRoom.adjustUI(onAdjustLight, rpcSuccessCB_2, errorCB_2);
+    }
+    function errorCB_1(err) {
+        console.log(err);
+    }
+    aRoom.comeIn(successCB_1, errorCB_1);
 });
